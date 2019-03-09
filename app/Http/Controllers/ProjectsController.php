f5f27d4f2a68d81be535976a05b87f599e52d012
Rob Pomerleau <pomerleau.robert@gmail.com>
2019-03-08 23:27:46 -0700

    Continue testing and adding to Projects

--- app/Http/Controllers/ProjectsController.php
+++ app/Http/Controllers/ProjectsController.php
@@ -8,12 +8,17 @@ use App\Project;
 class ProjectsController extends Controller
 {
     public function index() {
-        $projects =  Project::all();
-
+        $projects =  auth()->user()->projects;
+        
         return view('projects.index' , compact('projects'));
     }
     
     public function show(Project $project) {
+        
+        if(auth()->user()->isNot($project->owner)) {
+            abort(403);
+        }
+
         return view('projects.show' , compact('project'));
     }
 
@@ -23,8 +28,8 @@ class ProjectsController extends Controller
             'title' => 'required' , 
             'description' => 'required'
         ]);
-        
-        Project::create( $attributes );
+
+        auth()->user()->projects()->create($attributes);
 
         return redirect('projects');
     }
